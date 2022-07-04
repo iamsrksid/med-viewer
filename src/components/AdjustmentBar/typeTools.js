dd9254b840116fcc02aca18e9b62caceb8660a70
mohitKS777 <mohit.kumarsingh@softsensor.ai>
2022-07-04 13:41:20 +0530

    add draggable annotations bar

--- src/components/AdjustmentBar/typeTools.js
+++ src/components/AdjustmentBar/typeTools.js
@@ -1,9 +1,10 @@
 import React, { useEffect } from "react";
-import { Box, Text, HStack, Flex, Icon } from "@chakra-ui/react";
+import { Box, Text, HStack, Flex, Icon, VStack } from "@chakra-ui/react";
 import { fabric } from "openseadragon-fabricjs-overlay";
 import { SiTarget } from "react-icons/si";
 import { BsEraser } from "react-icons/bs";
 import { AiOutlineInfoCircle } from "react-icons/ai";
+import Draggable from "react-draggable";
 import Draw from "../Draw/draw";
 import Square from "../Shape/square";
 import Line from "../Shape/line";
@@ -23,44 +24,46 @@ const TypeTools = ({ viewerId, saveAnnotationsHandler }) => {
   };
 
   return (
-    <Flex direction="column" paddingTop="5px">
-      <Flex h="5px" bgColor="rgba(236, 236, 236, 1)" />
-      <HStack px="16px" h="42px" bgColor="rgba(248, 248, 245, 1)">
-        <Line
-          viewerId={viewerId}
-          saveAnnotationsHandler={saveAnnotationsHandler}
-        />
-        <Typebutton
-          disabled
-          icon={<SiTarget size={18} color="rgba(21, 28, 37, 1)" />}
-        />
-        <Square
-          viewerId={viewerId}
-          saveAnnotationsHandler={saveAnnotationsHandler}
-        />
-        <Circle
-          viewerId={viewerId}
-          saveAnnotationsHandler={saveAnnotationsHandler}
-        />
-        <Polygon
-          viewerId={viewerId}
-          saveAnnotationsHandler={saveAnnotationsHandler}
-        />
-        <Draw
-          viewerId={viewerId}
-          saveAnnotationsHandler={saveAnnotationsHandler}
-        />
-        <Typebutton
-          disabled
-          icon={<BsEraser size={18} color="rgba(21, 28, 37, 1)" />}
-          label="Eraser"
-        />
-        <Typebutton
-          disabled
-          icon={<AiOutlineInfoCircle size={18} color="rgba(21, 28, 37, 1)" />}
-        />
-      </HStack>
-    </Flex>
+    <Draggable>
+      <Flex direction="column" paddingTop="5px" pos="fixed" left="0">
+        <Flex h="12px" bgColor="rgba(236, 236, 236, 1)" cursor="crosshair" />
+        <VStack px="16px" bgColor="rgba(248, 248, 245, 1)">
+          <Line
+            viewerId={viewerId}
+            saveAnnotationsHandler={saveAnnotationsHandler}
+          />
+          <Typebutton
+            disabled
+            icon={<SiTarget size={18} color="rgba(21, 28, 37, 1)" />}
+          />
+          <Square
+            viewerId={viewerId}
+            saveAnnotationsHandler={saveAnnotationsHandler}
+          />
+          <Circle
+            viewerId={viewerId}
+            saveAnnotationsHandler={saveAnnotationsHandler}
+          />
+          <Polygon
+            viewerId={viewerId}
+            saveAnnotationsHandler={saveAnnotationsHandler}
+          />
+          <Draw
+            viewerId={viewerId}
+            saveAnnotationsHandler={saveAnnotationsHandler}
+          />
+          <Typebutton
+            disabled
+            icon={<BsEraser size={18} color="rgba(21, 28, 37, 1)" />}
+            label="Eraser"
+          />
+          <Typebutton
+            disabled
+            icon={<AiOutlineInfoCircle size={18} color="rgba(21, 28, 37, 1)" />}
+          />
+        </VStack>
+      </Flex>
+    </Draggable>
   );
 };
 
