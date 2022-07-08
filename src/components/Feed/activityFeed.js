967567880322f0c1639dc5ccc54497be4d5b51ca
mohitKS777 <mohit.kumarsingh@softsensor.ai>
2022-07-08 19:07:22 +0530

    update feed UI

--- src/components/Feed/activityFeed.js
+++ src/components/Feed/activityFeed.js
@@ -15,8 +15,10 @@ import {
   AccordionButton,
   AccordionPanel,
   AccordionIcon,
+  Divider,
 } from "@chakra-ui/react";
 import "../../styles/scrollBar.css";
+import { Scrollbars } from "react-custom-scrollbars";
 import { AiFillLock, AiOutlineEye } from "react-icons/ai";
 import { BiDotsVertical } from "react-icons/bi";
 import { MdModeEditOutline } from "react-icons/md";
@@ -31,12 +33,15 @@ import NecrosisIcon from "../../assets/images/NecrosisIcon.svg";
 
 const ActivityFeed = ({ userInfo, viewerId, handlePopup, popup }) => {
   const { fabricOverlayState, setFabricOverlayState } = useFabricOverlayState();
-  const { fabricOverlay, activityFeed, viewer } =
+  const { fabricOverlay, activityFeed, viewer, slideName } =
     fabricOverlayState?.viewerWindow[viewerId];
 
   const scrollbar = useRef(null);
   const { isOpen, onClose, onOpen } = useDisclosure();
   const [annotationObject, setAnnotationObject] = useState(null);
+  const [annotationDetails, setAnnotationsDetails] = useState(null);
+
+  console.log(activityFeed);
 
   useEffect(() => {
     if (scrollbar.current) scrollbar.current.scrollToBottom();
@@ -115,17 +120,20 @@ const ActivityFeed = ({ userInfo, viewerId, handlePopup, popup }) => {
   };
 
   return (
-    <Box
+    <Flex
       w="16.52vw"
+      h="89.2%"
       pos="absolute"
       right="0"
       zIndex={100}
-      h="89.5%"
       background="#FCFCFC"
       boxShadow="-1px 0px 2px rgba(176, 200, 214, 0.3)"
-      overflow="scroll"
+      direction="column"
+      overflowY="auto"
+      overflowX="hidden"
     >
-      {/* <Scrollbars
+      {/* <>
+      <Scrollbars
         ref={scrollbar}
         style={{ width: "100%", height: "100%", borderWidth: "0px" }}
         renderThumbVertical={(props) => <div className="thumb-vertical" />}
@@ -141,118 +149,123 @@ const ActivityFeed = ({ userInfo, viewerId, handlePopup, popup }) => {
           onClick={() => handlePopup(!popup)}
           _hover={{ cursor: "pointer" }}
         >
-          X
+          x
         </Text>
       </Flex>
-      <Accordion allowMultiple pb="2vh">
-        <AccordionItem>
-          <h2>
-            <AccordionButton>
-              <Box flex="1" textAlign="left">
-                Annotation Details
+      <HStack
+        fontSize="0.729vw"
+        spacing="1vw"
+        w="100%"
+        h="4vh"
+        marginStart="0.8vw"
+      >
+        <Text>Image Info</Text>
+        <Text>Key Points</Text>
+        <Text color="blue">Annotation</Text>
+      </HStack>
+
+      <Flex
+        direction="column"
+        marginStart="0.8vw"
+        pt="3px"
+        h="47%"
+        overflowY="auto"
+      >
+        <Text fontSize="1vw" pb="3px">
+          Annotation Details
+        </Text>
+        <Flex direction="column">
+          {activityFeed.map((feed) => {
+            return (
+              <Box>
+                {feed?.type === "rect" ? (
+                  <Flex
+                    pb="0.5vh"
+                    borderBottom="1px solid #F6F6F6"
+                    cursor="pointer"
+                    onClick={() => setAnnotationsDetails(feed)}
+                  >
+                    <Image src={StromaIcon} />
+                    <Text ml="0.8vw">Stroma</Text>
+                  </Flex>
+                ) : feed?.type === "polygon" ? (
+                  <Flex
+                    pb="0.5vh"
+                    borderBottom="1px solid #F6F6F6"
+                    cursor="pointer"
+                    onClick={() => setAnnotationsDetails(feed)}
+                  >
+                    <Image src={TumorIcon} />
+                    <Text ml="0.5vw">Tumor</Text>
+                  </Flex>
+                ) : feed?.type === "ellipse" ? (
+                  <Flex
+                    pb="0.5vh"
+                    borderBottom="1px solid #F6F6F6"
+                    cursor="pointer"
+                    onClick={() => setAnnotationsDetails(feed)}
+                  >
+                    <Image src={ImmuneCellsIcon} />
+                    <Text ml="0.8vw">Immune Cells</Text>
+                  </Flex>
+                ) : (
+                  <Flex
+                    pb="0.5vh"
+                    borderBottom="1px solid #F6F6F6"
+                    cursor="pointer"
+                    onClick={() => setAnnotationsDetails(feed)}
+                  >
+                    <Image src={NecrosisIcon} />
+                    <Text ml="0.8vw">Necrosis</Text>
+                  </Flex>
+                )}
               </Box>
-              <AccordionIcon />
-            </AccordionButton>
-          </h2>
-          <AccordionPanel pb={4}>
-            <Flex pb="0.5vh">
-              <Image src={TumorIcon} />
-              <Text ml="0.5vw">Tumor</Text>
-            </Flex>
-            <Flex pb="0.5vh">
-              <Image src={StromaIcon} />
-              <Text ml="0.8vw">Stroma</Text>
-            </Flex>
-            <Flex pb="0.5vh">
-              <Image src={ImmuneCellsIcon} />
-              <Text ml="0.8vw">Immune Cells</Text>
-            </Flex>
-            <Flex pb="0.5vh">
-              <Image src={NecrosisIcon} />
-              <Text ml="0.8vw">Necrosis</Text>
-            </Flex>
-          </AccordionPanel>
-        </AccordionItem>
-      </Accordion>
-      <VStack w="100%" align="center" right={0}>
-        {activityFeed.map((activity, index) => (
-          <Box
-            key={index}
-            w="100%"
-            px="5px"
-            pt="5px"
-            pb="2px"
-            color="black"
-            backgroundColor="#FCFCFC"
-            cursor="pointer"
-            onClick={() => handleClick(activity)}
-          >
-            <HStack pl={2} align="flex-start" spacing={4}>
-              <Image
-                borderWidth="2px"
-                backgroundColor="white"
-                crossOrigin="anonymous"
-                width="40%"
-                height="6em"
-                objectFit="contain"
-                src={activity?.image}
-              />
-              <VStack>
-                <Flex w="100%" verticalAlign="top" fontWeight="bold">
-                  <Text color="#151C25" fontFamily="inter" fontSize="14px">
-                    {`${userInfo?.firstName} ${userInfo?.lastName}`}
-                  </Text>
-                  <Spacer />
-                </Flex>
-              </VStack>
-            </HStack>
-            <VStack w="100%" align="flex-start" justify="center" pl={2} my={3}>
-              <HStack
-                w="100%"
-                justify={activity?.object?.text ? "space-between" : "normal"}
-                pr={5}
-                spacing={4}
-              >
-                <Text fontSize="14px" fontFamily="inter" fontWeight="bold">
-                  {activity?.object?.text ? "Description" : "No Description"}
-                </Text>
-                {activity?.object && activity?.object?.isExist
-                  ? // <EditTextButton activity={activity} />
-                    null
-                  : null}
-              </HStack>
-              {activity?.object?.text ? (
-                <Box height="2.5em" width="100%">
-                  <Text fontSize="xs" textAlign="left" color="#151C25">
-                    {activity?.object?.text}
-                  </Text>
-                </Box>
-              ) : null}
-            </VStack>
-            <HStack
-              color="black"
-              pl={2}
-              pr={5}
-              py={2}
-              mb={1}
-              spacing={4}
-              cursor="pointer"
-              justify="flex-end"
-              borderTop="2px solid #E4E5E8 "
-            >
-              <Icon as={AiOutlineEye} w={4} h={4} />
-              <Icon as={AiFillLock} w={4} h={4} />
-              <Icon
-                as={RiDeleteBin6Line}
-                w={4}
-                h={4}
-                onClick={(e) => deleteObject(e, activity)}
-              />
-              <Icon as={BiDotsVertical} w={4} h={4} />
-            </HStack>
-          </Box>
-        ))}
-      </VStack>
+            );
+          })}
+        </Flex>
+      </Flex>
+      <Flex h="50%" overflowY="auto" fontSize="14px" direction="column">
+        <Box h="10px" background="#F6F6F6" w="100%" />
+        <Text pt="1.2vh" pb="1.2vh" fontSize="1vw" marginStart="0.8vw">
+          Annotation Values
+        </Text>
+
+        <HStack borderBottom="1px solid #F6F6F6" pb="0.5vw" marginStart="0.8vw">
+          <Text minW="35%">Slide Name:</Text>
+          <Text>{slideName}</Text>
+        </HStack>
+        <HStack marginStart="0.8vw" borderBottom="1px solid #F6F6F6" pb="0.5vw">
+          <Text minW="35%">Annotation:</Text>
+          <Text>{annotationDetails ? annotationDetails.type : "NULL"}</Text>
+        </HStack>
+        <HStack marginStart="0.8vw" borderBottom="1px solid #F6F6F6" pb="0.5vw">
+          <Text minW="35%">Tag:</Text>
+          <Text>
+            {annotationDetails
+              ? annotationDetails.type === "rect"
+                ? "Stroma"
+                : annotationDetails.type === "polygon"
+                ? "Tumor"
+                : annotationDetails.type === "ellipse"
+                ? "Immune Cells"
+                : "Necrosis"
+              : "NULL"}
+          </Text>
+        </HStack>
+        <HStack marginStart="0.8vw" borderBottom="1px solid #F6F6F6" pb="0.5vw">
+          <Text minW="35%">Centroid Xum:</Text>
+          <Text>undefined</Text>
+        </HStack>
+        <HStack marginStart="0.8vw" borderBottom="1px solid #F6F6F6" pb="0.5vw">
+          <Text minW="35%">Centroid Yum:</Text>
+          <Text>undefined</Text>
+        </HStack>
+        <HStack marginStart="0.8vw" borderBottom="1px solid #F6F6F6" pb="0.5vw">
+          <Text minW="35%">length:</Text>
+          <Text>undefined</Text>
+        </HStack>
+      </Flex>
+
       {/* </Scrollbars> */}
       <EditText
         isOpen={isOpen}
@@ -260,9 +273,7 @@ const ActivityFeed = ({ userInfo, viewerId, handlePopup, popup }) => {
         handleClose={onClose}
         handleSave={handleSave}
       />
-
-      {/* <Text display={popup ? "flex" : "none"}>Hello</Text> */}
-    </Box>
+    </Flex>
   );
 };
 
