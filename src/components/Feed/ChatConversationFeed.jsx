f6d8ed2e1e95bfea80f10041c5dd899b7efb5d1f
adarshkcc <72060392+adarshkcc@users.noreply.github.com>
2023-01-25 13:02:49 +0530

    Update ChatConversationFeed.jsx

--- src/components/Feed/ChatConversationFeed.jsx
+++ src/components/Feed/ChatConversationFeed.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import React,{ useEffect, useRef, useState } from "react";
 import { useLazyQuery, useMutation, useSubscription } from "@apollo/client";
 import { Mention, MentionsInput } from "react-mentions";
 import {
